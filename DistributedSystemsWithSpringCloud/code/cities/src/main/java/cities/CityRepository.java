ac25cf8b8179075755f76eb6b4ea2eb8bdb1d3f6
Matt Stine <mstine@gopivotal.com>
2015-01-14 21:48:35 -0800

    add cities demo app

---
+++ DistributedSystemsWithSpringCloud/code/cities/src/main/java/cities/CityRepository.java
@@ -0,0 +1,8 @@
+package cities;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
+public interface CityRepository extends PagingAndSortingRepository<City, Long> {
+}
