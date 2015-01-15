4631b278797052e9e7fd3334068336b481f8c60c
Matt Stine <mstine@gopivotal.com>
2015-01-15 12:26:53 -0800

    make producer register properly for localhost

--- DistributedSystemsWithSpringCloud/code/producer/app.groovy
+++ DistributedSystemsWithSpringCloud/code/producer/app.groovy
@@ -1,11 +1,15 @@
 @EnableDiscoveryClient
 @RestController
+@Log
 public class Application {
 
   int counter = 0
 
   @RequestMapping("/")
   String produce() {
-    "{\"value\": ${counter++}}"
+    counter++
+    log.info("Produced a value: ${counter}")
+
+    "{\"value\": ${counter}}"
   }
 }
