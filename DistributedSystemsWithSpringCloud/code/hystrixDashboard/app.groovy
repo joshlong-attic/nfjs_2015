3564c953a7f6f793b17e143962081973e005c14a
Matt Stine <mstine@gopivotal.com>
2015-01-15 15:17:42 -0800

    add hystrixDashboard demo app

---
+++ DistributedSystemsWithSpringCloud/code/hystrixDashboard/app.groovy
@@ -0,0 +1,7 @@
+@Grab("org.springframework.cloud:spring-cloud-starter-hystrix-dashboard:1.0.0.RC1")
+
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+
+@EnableHystrixDashboard
+class HystrixDashboard {
+}
