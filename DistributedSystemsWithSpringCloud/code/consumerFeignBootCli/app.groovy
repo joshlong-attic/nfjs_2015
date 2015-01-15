620c64c72566f574a2d84645092cc7b6ccdd4103
Matt Stine <mstine@gopivotal.com>
2015-01-15 13:40:55 -0800

    add broken consumerFeignBootCli for troubleshooting

---
+++ DistributedSystemsWithSpringCloud/code/consumerFeignBootCli/app.groovy
@@ -0,0 +1,37 @@
+package producer
+
+@Grab("org.springframework.cloud:spring-cloud-starter-feign:1.0.0.BUILD-SNAPSHOT")
+@Grab("org.springframework.cloud:spring-cloud-starter-eureka:1.0.0.BUILD-SNAPSHOT")
+
+import org.springframework.cloud.netflix.eureka.EurekaClient
+import org.springframework.cloud.netflix.feign.FeignClient
+import org.springframework.cloud.netflix.feign.FeignClientScan
+
+@EnableEurekaClient
+@FeignClientScan
+@RestController
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+  @Autowired
+  ProducerClient client
+
+  @RequestMapping("/")
+  String consume() {
+    ProducerResponse response = client.getValue()
+
+    "{\"value\": ${response.value}"
+  }
+}
+
+@FeignClient("producer")
+public interface ProducerClient {
+  @RequestMapping(method = RequestMethod.GET, value = "/")
+  ProducerResponse getValue()
+}
+
+public class ProducerResponse {
+  Integer value
+}
